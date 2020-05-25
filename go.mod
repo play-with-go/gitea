f5f3913c14db68daeb72c0dca478b8efa7dce0cd
Paul Jolly <paul@myitcv.io>
2020-05-25 17:20:25 +0100

    Remove old repos; use dockexec (#1)

--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/play-with-go/gitea
 go 1.15
 
 require (
-	code.gitea.io/sdk/gitea v0.11.3
+	code.gitea.io/sdk/gitea v0.12.0
 	github.com/google/go-github/v31 v31.0.0
-	github.com/kr/pretty v0.2.0
+	github.com/kr/pretty v0.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/retry.v1 v1.0.3
 )
--- setup.sh
+++ setup.sh
@@ -26,8 +26,10 @@ docker-compose run --rm -u git gitea gitea admin create-user --username root --p
 
 docker-compose up -d
 
+dockexec="go run mvdan.cc/dockexec buildpack-deps@sha256:ec0e9539673254d0cb1db0de347905cdb5d5091df95330f650be071a7e939420 --network=gitea_gitea --rm -e PLAYWITHGODEV_ROOT_USER -e PLAYWITHGODEV_ROOT_PASSWORD -e PLAYWITHGODEV_GITHUB_USER -e PLAYWITHGODEV_GITHUB_PAT"
+
 # Wait for API to serve
-go run -exec $PWD/dockexec.sh . wait
+go run -exec "$dockexec" . wait
 
 # Create userguides org
-go run -exec $PWD/dockexec.sh . pre
+go run -exec "$dockexec" . pre
