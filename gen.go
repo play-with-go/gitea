99da3fcf74b9e871fcb54453c90e0723d33d67dd
Paul Jolly <paul@myitcv.io>
2020-06-10 16:25:50 +0100

    Simply CUE generation steps (#10)

--- gen.go
+++
@@ -1,4 +0,0 @@
-package main
-
-//go:generate go run cuelang.org/go/cmd/cue cmd vendorgithubschema github.com/play-with-go/gitea
-//go:generate go run cuelang.org/go/cmd/cue cmd -t workflowsDir=./.github/workflows gengithub ./internal/ci
