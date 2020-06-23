93b3a83a9f5d0e85d63a0f52aedb2c18e7f2307f
Paul Jolly <paul@myitcv.io>
2020-06-23 12:18:22 +0100

    Use github.com/myitcv/docker-compose
    Using github.com/myitcv/docker-compose simplifies the use of
    docker-compose in this project, but also enables other projects to
    compose this docker-compose.yml into their own setups.
    
    As part of this we ensure that the _scripts/setup.sh script can be run
    in the context of another project. The other project does that by
    setting COMPOSE_FILE to the "environment" of compose config files, a
    list that can be extended by any call to docker-compose using the -f
    flag.

--- _scripts/common.bash
+++ _scripts/common.bash
@@ -1,13 +1,3 @@
-# Create a temporary docker-compose script and update PATH
-td=$(mktemp -d)
-# trap "rm -rf $td" EXIT
-dc=$(which docker-compose)
-cat <<EOD > $td/docker-compose
-#!/usr/bin/env bash
-
-$dc -f docker-compose.yml -f docker-compose-playwithgo.yml "\$@"
-EOD
-chmod +x $td/docker-compose
-export PATH="$td:$PATH"
-
+root="$( command cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../"
+export COMPOSE_FILE="${COMPOSE_FILE:-}:$root/docker-compose.yml:$root/docker-compose-playwithgo.yml"
 dockexec="go run mvdan.cc/dockexec -compose playwithgo -e PLAYWITHGODEV_ROOT_USER -e PLAYWITHGODEV_ROOT_PASSWORD -e PLAYWITHGODEV_GITHUB_USER -e PLAYWITHGODEV_GITHUB_PAT"
