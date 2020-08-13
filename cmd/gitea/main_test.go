c2d1dd512825fe48fa08d5356149f3113b20a7ad
Paul Jolly <paul@myitcv.io>
2020-08-13 09:50:09 +0100

    Add server command to make gitea_prestep an HTTP service

--- cmd/gitea/main_test.go
+++ cmd/gitea/main_test.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 	"testing"
+
+	"github.com/play-with-go/preguide"
 )
 
 const (
@@ -15,20 +18,45 @@ const (
 
 type testRunner struct {
 	*testing.T
-	root string
+	root           string
+	envComposeFile string
+	envGoModCache  string
 }
 
 func newTestRunner(t *testing.T) *testRunner {
-	var stdout, stderr bytes.Buffer
-	cmd := exec.Command("go", "list", "-m", "-f", "{{.Dir}}", "github.com/play-with-go/gitea")
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	if err := cmd.Run(); err != nil {
-		t.Fatalf("failed to get root of module: %v\n%s", err, stderr.Bytes())
+	var listOut, listErr bytes.Buffer
+	list := exec.Command("go", "list", "-m", "-f", "{{.Dir}}", "github.com/play-with-go/gitea")
+	list.Stdout = &listOut
+	list.Stderr = &listErr
+	if err := list.Run(); err != nil {
+		t.Fatalf("failed to get root of module: %v\n%s", err, listErr.Bytes())
 	}
+	root := strings.TrimSpace(listOut.String())
+	composeFiles := []string{
+		os.Getenv(envComposeFile),
+		filepath.Join(root, "docker-compose.yml"),
+		filepath.Join(root, "docker-compose-playwithgo.yml"),
+	}
+	var goenv struct {
+		GOPATH string
+	}
+	var envOut, envErr bytes.Buffer
+	env := exec.Command("go", "env", "-json")
+	env.Stdout = &envOut
+	env.Stderr = &envErr
+	if err := env.Run(); err != nil {
+		t.Fatalf("failed to get go env: %v\n%s", err, envErr.Bytes())
+	}
+	if err := json.Unmarshal(envOut.Bytes(), &goenv); err != nil {
+		t.Fatalf("failed to unmarshal go env output (%q): %v", envOut.Bytes(), err)
+	}
+	gopath0 := strings.Split(goenv.GOPATH, string(os.PathListSeparator))[0]
+
 	return &testRunner{
-		T:    t,
-		root: strings.TrimSpace(stdout.String()),
+		T:              t,
+		root:           root,
+		envComposeFile: strings.Join(composeFiles, string(os.PathListSeparator)),
+		envGoModCache:  filepath.Join(gopath0, "pkg", "mod"),
 	}
 }
 
@@ -46,34 +74,36 @@ func TestNewUser(t *testing.T) {
 		tr.mustRunDockerCompose("down", "-t", "0", "-v")
 	})
 
-	tr.mustRun("go", "run", "github.com/play-with-go/gitea/cmd/gitea", "setup")
-	tr.mustRun("./_scripts/newuser.sh", "run", "-test")
-}
+	tr.mustRunCmd(exec.Command("go", "run", "github.com/play-with-go/gitea/cmd/gitea", "setup"))
+	newUser := exec.Command("go", "run", "github.com/play-with-go/gitea/cmd/gitea", "newuser")
+	newUser.Stdin = strings.NewReader(`{"Repos": [{"Var": "REPO1", "Pattern": "user*"}]}`)
+	outJSON, _ := tr.mustRunCmd(newUser)
 
-func (tr *testRunner) mustRun(cmd string, args ...string) string {
-	c := exec.Command(cmd, args...)
-	c.Dir = tr.root
-	return tr.mustRunCmd(c)
-}
+	var env preguide.PrestepOut
 
-func (tr *testRunner) mustRunDockerCompose(args ...string) string {
-	composeFiles := []string{
-		os.Getenv(envComposeFile),
-		filepath.Join(tr.root, "docker-compose.yml"),
-		filepath.Join(tr.root, "docker-compose-playwithgo.yml"),
+	// Verify we got some valid JSON back
+	if err := json.Unmarshal(outJSON, &env); err != nil {
+		t.Fatalf("failed to decode preguide.PrestepOut from %q: %v", outJSON, err)
 	}
+}
+
+func (tr *testRunner) mustRunDockerCompose(args ...string) ([]byte, []byte) {
 	c := exec.Command("docker-compose", args...)
 	c.Dir = tr.root
 	c.Env = append(os.Environ(),
-		"COMPOSE_FILE="+strings.Join(composeFiles, string(os.PathListSeparator)),
+		"COMPOSE_FILE="+tr.envComposeFile,
+		"GOMODCACHE="+tr.envGoModCache,
 	)
 	return tr.mustRunCmd(c)
 }
 
-func (tr *testRunner) mustRunCmd(cmd *exec.Cmd) string {
-	out, err := cmd.CombinedOutput()
+func (tr *testRunner) mustRunCmd(cmd *exec.Cmd) ([]byte, []byte) {
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
 	if err != nil {
-		tr.Fatalf("failed to run [%v]: %v\n%s", cmd, err, out)
+		tr.Fatalf("failed to run [%v]: %v\n%s", cmd, err, stderr.Bytes())
 	}
-	return string(out)
+	return stdout.Bytes(), stderr.Bytes()
 }
--- cmd/gitea/setup.go
+++ cmd/gitea/setup.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"strings"
 )
 
 func (r *runner) runSetup(args []string) error {
-	if err := r.preCmd.fs.Parse(args); err != nil {
-		return r.preCmd.usageErr("failed to parse flags: %v", err)
+	if err := r.setupCmd.fs.Parse(args); err != nil {
+		return r.setupCmd.usageErr("failed to parse flags: %v", err)
 	}
 	migrate := exec.Command("docker-compose", "exec", "-T", "-u", "git", "gitea", "gitea", "migrate")
 	out, err := migrate.CombinedOutput()
@@ -24,31 +20,15 @@ func (r *runner) runSetup(args []string) error {
 	out, err = adminUser.CombinedOutput()
 	check(err, "failed to run [%v]: %v\n%s", adminUser, err, out)
 
-	var stdout, stderr bytes.Buffer
-	rootDirCmd := exec.Command("go", "list", "-m", "-f", "{{.Dir}}", "github.com/play-with-go/gitea")
-	rootDirCmd.Stdout = &stdout
-	rootDirCmd.Stderr = &stderr
-	err = rootDirCmd.Run()
-	check(err, "failed to run [%v]: %v\n%s", rootDirCmd, err, stderr.Bytes())
-
-	self, err := os.Executable()
-	check(err, "failed to determine self: %v", err)
-
-	rootDir := strings.TrimSpace(stdout.String())
-
 	// Now run self in Docker
-	org := exec.Command("docker-compose",
-		"-f", filepath.Join(rootDir, "docker-compose.yml"),
-		"-f", filepath.Join(rootDir, "docker-compose-playwithgo.yml"),
-		"run",
-		"-v", fmt.Sprintf("%v:/init", self), "--entrypoint=/init",
-		"-v", fmt.Sprintf("%v:/workdir", rootDir), "--workdir=/workdir",
+	org := r.buildSelfDockerCmd(
 		"-e", "PLAYWITHGODEV_ROOT_USER",
 		"-e", "PLAYWITHGODEV_ROOT_PASSWORD",
 		"-e", "PLAYWITHGODEV_GITHUB_USER",
 		"-e", "PLAYWITHGODEV_GITHUB_PAT",
-		"playwithgo", "pre",
 	)
+	// Add the args to self
+	org.Args = append(org.Args, "pre")
 	out, err = org.CombinedOutput()
 	check(err, "failed to run [%v]: %v\n%s", org, err, out)
 
