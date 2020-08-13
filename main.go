4c1329d9da0c078d61b491799cd42bac6d1fca1f
Paul Jolly <paul@myitcv.io>
2020-08-13 06:32:06 +0100

    Test in a Go test (#19)
    Testing in a regular Go test is more robust than asking someone to run a
    set of bash commands.
    
    Add a setup command to replace the existing bash script.
    
    Remove the wait command as a precursor to further changes to create a
    server command, making gitea_prestep an HTTP endpoint.

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"text/template"
 	"time"
@@ -38,7 +39,7 @@ const (
 
 type runner struct {
 	*rootCmd
-	waitCmd    *waitCmd
+	setupCmd   *setupCmd
 	preCmd     *preCmd
 	newUserCmd *newUserCmd
 	client     *gitea.Client
@@ -72,8 +73,8 @@ func (r *runner) mainerr() (err error) {
 	}
 	cmd := args[0]
 	switch cmd {
-	case "wait":
-		return r.runWait(args[1:])
+	case "setup":
+		return r.runSetup(args[1:])
 	case "pre":
 		return r.runPre(args[1:])
 	case "newuser":
@@ -83,12 +84,58 @@ func (r *runner) mainerr() (err error) {
 	}
 }
 
-func (r *runner) runWait(args []string) error {
-	if err := r.waitCmd.fs.Parse(args); err != nil {
-		return r.waitCmd.usageErr("failed to parse flags: %v", err)
+func (r *runner) runSetup(args []string) error {
+	if err := r.preCmd.fs.Parse(args); err != nil {
+		return r.preCmd.usageErr("failed to parse flags: %v", err)
+	}
+	migrate := exec.Command("docker-compose", "run", "--rm", "-u", "git", "gitea", "gitea", "migrate")
+	out, err := migrate.CombinedOutput()
+	check(err, "failed to run [%v]: %v\n%s", migrate, err, out)
+
+	adminUser := exec.Command("docker-compose", "run", "--rm", "-u", "git", "gitea", "gitea", "admin", "create-user",
+		"--admin", "--username", os.Getenv("PLAYWITHGODEV_ROOT_USER"),
+		"--password", os.Getenv("PLAYWITHGODEV_ROOT_PASSWORD"), "--email", "blah@blah.com",
+	)
+	out, err = adminUser.CombinedOutput()
+	check(err, "failed to run [%v]: %v\n%s", adminUser, err, out)
+
+	var stdout, stderr bytes.Buffer
+	rootDirCmd := exec.Command("go", "list", "-f", "{{.Dir}}", "github.com/play-with-go/gitea")
+	rootDirCmd.Stdout = &stdout
+	rootDirCmd.Stderr = &stderr
+	err = rootDirCmd.Run()
+	check(err, "failed to run [%v]: %v\n%s", rootDirCmd, err, stderr.Bytes())
+
+	self, err := os.Executable()
+	check(err, "failed to determine self: %v", err)
+
+	rootDir := strings.TrimSpace(stdout.String())
+
+	// Now run self in Docker
+	org := exec.Command("docker-compose",
+		"-f", filepath.Join(rootDir, "docker-compose.yml"),
+		"-f", filepath.Join(rootDir, "docker-compose-playwithgo.yml"),
+		"run",
+		"-v", fmt.Sprintf("%v:/init", self), "--entrypoint=/init",
+		"-v", fmt.Sprintf("%v:/workdir", rootDir), "--workdir=/workdir",
+		"-e", "PLAYWITHGODEV_ROOT_USER",
+		"-e", "PLAYWITHGODEV_ROOT_PASSWORD",
+		"-e", "PLAYWITHGODEV_GITHUB_USER",
+		"-e", "PLAYWITHGODEV_GITHUB_PAT",
+		"playwithgo", "pre",
+	)
+	out, err = org.CombinedOutput()
+	check(err, "failed to run [%v]: %v\n%s", org, err, out)
+
+	return nil
+}
+
+func (r *runner) runPre(args []string) error {
+	if err := r.preCmd.fs.Parse(args); err != nil {
+		return r.preCmd.usageErr("failed to parse flags: %v", err)
 	}
 	// Try the version endpoint with backoff until success or timeout
-	wait, err := time.ParseDuration(*r.waitCmd.fWait)
+	wait, err := time.ParseDuration(*r.preCmd.fWait)
 	check(err, "failed to parse duration from -wait flag: %v", err)
 
 	strategy := retry.LimitTime(wait,
@@ -103,14 +150,9 @@ func (r *runner) runWait(args []string) error {
 			break
 		}
 	}
-	return err
-}
-
-func (r *runner) runPre(args []string) error {
-	if err := r.preCmd.fs.Parse(args); err != nil {
-		return r.preCmd.usageErr("failed to parse flags: %v", err)
+	if err != nil {
+		return err
 	}
-	var err error
 	_, err = r.client.CreateOrg(gitea.CreateOrgOption{
 		UserName:   UserGuidesRepo,
 		Visibility: "private",
