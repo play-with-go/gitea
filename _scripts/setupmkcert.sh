2e4a00866b53351f3d8ac6af983fe2a396a46c1a
Paul Jolly <paul@myitcv.io>
2020-08-19 14:33:32 +0100

    Make mkcert helper a simple script as part of gitea module
    Which means we gain a tool dependency, but that's life.

--- _scripts/setupmkcert.sh
+++ _scripts/setupmkcert.sh
@@ -3,12 +3,17 @@
 set -eu -o pipefail
 shopt -s inherit_errexit
 
-command cd "$( command cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/mkcert"
+# TODO: not ideal that this is a dependency of the main module
 go install github.com/FiloSottile/mkcert
 mkcert -install
+
+setenv() {
+	echo "::set-env name=$1::$(cat $2 | sed ':a;N;$!ba;s/%/%25/g' |  sed ':a;N;$!ba;s/\r/%0D/g' | sed ':a;N;$!ba;s/\n/%0A/g')"
+}
 
 td=$(mktemp -d)
 trap "rm -rf $td" EXIT
 mkcert -cert-file $td/cert.pem -key-file $td/key.pem play-with-go.dev "*.play-with-go.dev"
-go run . $td
 
+setenv PLAYWITHGODEV_CERT_FILE $td/cert.pem
+setenv PLAYWITHGODEV_KEY_FILE $td/key.pem
