838abe6767c31c7b1df0a35685ca849b63027f21
Paul Jolly <paul@myitcv.io>
2020-09-04 17:42:43 +0100

    ci: github.com/play-with-go/... repos are no longer private (#48)

--- _scripts/env.sh
+++ _scripts/env.sh
@@ -5,8 +5,6 @@ shopt -s inherit_errexit
 
 source "$( command cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/env_common.bash
 
-$export GOPRIVATE github.com/play-with-go/*
-
 modRoot="$(go list -m -f {{.Dir}})"
 
 GOBIN=$modRoot/.bin go install github.com/myitcv/docker-compose
