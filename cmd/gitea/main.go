564ad2f6eb28dddd6fdd45d8217c197d7fd4ef4a
Paul Jolly <paul@myitcv.io>
2020-09-07 11:50:23 +0100

    general: add license
    Also:
    
    * pin to latest preguide
    * use latest playwithgo image for prestep

--- cmd/gitea/main.go
+++ cmd/gitea/main.go
@@ -1,3 +1,7 @@
+// Copyright 2020 The play-with-go.dev Authors. All rights reserved.  Use of
+// this source code is governed by a BSD-style license that can be found in the
+// LICENSE file.
+
 // gitea is a helper to work with a gitea instance
 package main
 
